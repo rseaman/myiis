0dcda701dcb4e9ad7b27439823f0ac31e651f40e
rseaman <riley.seaman@gmail.com>
2017-11-01 20:35:31 +0000

    Adding node info to server site, incrementing minor version

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,13 @@ powershell_script 'Install IIS' do
 end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello World!</h1>'
+  content <<-EOH
+    <h1>Hello World!</h1>
+    <h2>PLATFORM: #{node['platform']}</h2>
+    <h2>HOSTNAME: #{node['hostname']}</h2>
+    <h2>MEMORY:   #{node['memory']['total']}</h2>
+    <h2>CPU MHz:  #{node['cpu']['0']['mhz']}</h2>
+    EOH
 end
 
 service 'w3svc' do
