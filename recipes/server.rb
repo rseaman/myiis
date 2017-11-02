2119056fc7a9fb52469a7b68b01899d788869bbd
rseaman <riley.seaman@gmail.com>
2017-11-02 13:51:55 +0000

    v0.2.1: Using template resource in place of file

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,14 +2,8 @@ powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content <<-EOH
-    <h1>Hello World!</h1>
-    <h2>PLATFORM: #{node['platform']}</h2>
-    <h2>HOSTNAME: #{node['hostname']}</h2>
-    <h2>MEMORY:   #{node['memory']['total']}</h2>
-    <h2>CPU MHz:  #{node['cpu']['0']['mhz']}</h2>
-    EOH
+template 'C:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
